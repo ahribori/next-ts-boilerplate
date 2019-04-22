c05c9172ded089cd9c04f6d0dcfc425c31ac99cf
ahribori <ahribori@gmail.com>
2019-04-23 00:42:49 +0900

    SSR cache

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 const nextRoutes = require('next-routes');
 
-const routes = nextRoutes().add({ name: 'index', pattern: '/', page: '/index' });
+const routes = nextRoutes()
+  .add({ name: 'index', pattern: '/', page: '/index' })
+  .add({ name: 'event', pattern: '/event/:eventId', page: '/event' });
 
 export const Link = routes.Link;
 export default routes;
