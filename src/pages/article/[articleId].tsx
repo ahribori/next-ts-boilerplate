bca7a2f9fd4469a380620d8bf82b64016166f45a
ahribori <ahribori@gmail.com>
2019-07-12 00:52:49 +0900

    Fetch Data & SSR

--- src/pages/article/[articleId].tsx
+++ src/pages/article/[articleId].tsx
@@ -1,11 +1,39 @@
 import React from 'react';
+import { NextPage } from 'next';
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
-const Article = () => {
+interface Article {
+  id: string;
+  title: string;
+  content: string;
+}
+
+interface IProps {
+  article: Article;
+}
+
+const Article: NextPage<IProps> = ({ article }) => {
   const router = useRouter();
   const { articleId } = router.query;
 
-  return <div>ArticleId: {articleId}</div>;
+  return (
+    <div>
+      ArticleId: {articleId}
+      <main>
+        <h1>{article.title}</h1>
+        <h3>{article.content}</h3>
+      </main>
+    </div>
+  );
+};
+
+Article.getInitialProps = async ({ query }) => {
+  const { articleId } = query;
+  const articleRequest = await axios.get(`http://localhost:3000/api/article/${articleId}`);
+  return {
+    article: articleRequest.data,
+  };
 };
 
 export default Article;
